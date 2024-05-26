ef345651a6f5720e8cc592332a046a495d447888
Rümeysa Çelik <rmysclk61@icloud.com>
2024-05-27 01:15:46 +0300

    Update auth.js

--- public/components/auth.js
+++ public/components/auth.js
@@ -5,15 +5,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 // Firebase yapılandırması
-const firebaseConfig = {
-    apiKey: "AIzaSyDybFDHGYgaf_aCUksbwTvzKoodBCT0axc",
-    authDomain: "cnninterface.firebaseapp.com",
-    projectId: "cnninterface",
-    storageBucket: "cnninterface.appspot.com",
-    messagingSenderId: "757886726589",
-    appId: "1:757886726589:web:0031829f0fea0a915f62d4",
-    measurementId: "G-XWZCZYKF61"
-};
 
 // Firebase uygulamasını başlatma
 const firebaseApp = initializeApp(firebaseConfig);
